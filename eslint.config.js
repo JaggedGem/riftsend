a06775f61ff3a41da2cda75b35c4b678d96b403c
JaggedGem <orletchi.bogdan2009@gmail.com>
2026-07-18 01:36:00 +0300

    chore(tooling): implement tooling further and finishing up everything

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["**/dist", "**/node_modules", "**/coverage"]),
+  globalIgnores(["**/dist", "**/node_modules", "**/coverage", "**/*.tsbuildinfo"]),
 
   {
     files: ["**/*.{ts,tsx}"],
